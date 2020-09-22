8d057b7d47dd524a43e23d3a04d76d15c93da579
Sophia Hammer <kontakt@sophia-hammer.de>
2020-09-22 06:37:54 +0200

    nav bar done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import 'bulma/css/bulma.css';
+
+import Navbar from './navbar/Navbar';
+import FormField from './formfield/FormField';
+
 
 const App = () => {
-  return <h1>Bulma</h1>;
+
+  return (
+<Fragment>
+  <Navbar/>
+  <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+</Fragment>
+  )
 };
 
 export default App;
---
+++ src/formfield/FormField.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function FormField(props) {
+    
+            return (
+              <div className='field'>
+                <label className='label'>{props.label}</label>
+                // some other code goes here
+              </div>
+            );
+            };
+
+
+
